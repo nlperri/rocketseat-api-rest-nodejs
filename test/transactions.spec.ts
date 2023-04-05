3941183f514a41a70d8edadaba4889476d1b2892
natalia <lnataliaperri@gmail.com>
2023-04-05 14:30:02 -0300

    feat: create transactions list test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,10 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
+
+//it.only
+//it.todo
+//it.skip
 
 describe('Transactions route,', () => {
   beforeAll(async () => {
@@ -20,5 +24,31 @@ describe('Transactions route,', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  // todo teste deve obrigatoriamente se excluir de qualquer contexto, logo não deve depender de outro teste. ao criar algum teste, deve partir do princípio que outros testes não existem!!! ou seja, para testar a listagem de transações, deve se criar a transação.
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
